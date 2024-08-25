69d8323e938754a706dc013ec4bd27959db2ac23
Adesh Tamrakar <adesht@outlook.com>
2024-08-25 10:47:27 +0530

    Added redis caching for modules

--- app/api/permissions/modules/update/route.ts
+++ app/api/permissions/modules/update/route.ts
@@ -4,6 +4,7 @@ import { authOptions } from "@/app/api/auth/[...nextauth]/route";
 import { and, eq, inArray, sql } from "drizzle-orm";
 import { NextRequest } from "next/server";
 import { role_modules_mapInHrm } from "drizzle/schema";
+import { kv } from '@vercel/kv';
 
 export async function POST(request: NextRequest) {
   const session = await getServerSession(authOptions);
@@ -18,6 +19,8 @@ export async function POST(request: NextRequest) {
   }
   const role_id = session.user.id;
   const tenant_id = session.user.tenant_id;
+  const userId = session.user.id;
+  const redisKey = process.env.REDIS_PREFIX + userId+"-"+tenant_id+"-modules";
 
   const modulesInsertValues = data.modules.notAllowed.map( (id:number) => ({
     module_id: id,
@@ -30,6 +33,8 @@ export async function POST(request: NextRequest) {
   .onConflictDoNothing();
   
   await db.delete(role_modules_mapInHrm).where(and(eq(role_modules_mapInHrm.role_id, data.roleId), eq(role_modules_mapInHrm.tenant_id, tenant_id), inArray(role_modules_mapInHrm.module_id, data.modules.allowed)))
+
+  await kv.json.forget(redisKey);
 
   return Response.json({message: "Updated"}, {status: 201});
 }
